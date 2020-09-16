f7bf0f5b7cafc6b4f9e1874c3b9fb7e5e8e8fc7c
lethalic <enciso.arnulfo@gmail.com>
2020-09-16 10:22:10 +0800

    init

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name','PCIS')}}</title>
+
+    </head>
+    <body>
+        <h1> ABOUT US</h1>
+        <p> This is the About page </p>
+        <p> This is the sample nice </p>
+    </body>
+</html>
